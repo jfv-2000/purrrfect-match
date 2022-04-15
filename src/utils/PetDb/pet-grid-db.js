83d037fe7f7eb4749fd276a0747b34b26b3c1421
cpereira00 <c_pereira00@outlook.com>
2022-04-15 16:49:20 -0400

    added animals to databases

--- src/utils/PetDb/pet-grid-db.js
+++ src/utils/PetDb/pet-grid-db.js
@@ -1,11 +1,11 @@
 const Pets = [
     {name: "Harry", image: "https://img.dog-learn.com/dog-breeds/siberian-husky/siberian-husky-sz6.jpg"},
-    {name: "Harry", image: "https://img.dog-learn.com/dog-breeds/siberian-husky/siberian-husky-sz6.jpg"},
-    {name: "Harry", image: "https://img.dog-learn.com/dog-breeds/siberian-husky/siberian-husky-sz6.jpg"},
-    {name: "Harry", image: "https://img.dog-learn.com/dog-breeds/siberian-husky/siberian-husky-sz6.jpg"},
-    {name: "Harry", image: "https://img.dog-learn.com/dog-breeds/siberian-husky/siberian-husky-sz6.jpg"},
-    {name: "Harry", image: "https://img.dog-learn.com/dog-breeds/siberian-husky/siberian-husky-sz6.jpg"},
-    {name: "Harry", image: "https://img.dog-learn.com/dog-breeds/siberian-husky/siberian-husky-sz6.jpg"},
+    {name: "Thinsel", image: "https://uploads-ssl.webflow.com/5ff0b3326e5782a256714165/624375b28c9adcbe3005d3a2_thinsel22.4.jpg"},
+    {name: "Meiko", image: "https://uploads-ssl.webflow.com/5ff0b3326e5782a256714165/62251ba486d8b60f37000ad3_Meiko22.3.jpg"},
+    {name: "James", image: "https://uploads-ssl.webflow.com/5ff0b3326e5782a256714165/624ef2660d42c6646d532664_Miko22.2.jpg"},
+    {name: "Gustave", image: "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/55149182/2/?bust=1648596181&width=1080"},
+    {name: "Grizzley", image: "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/55152551/1/?bust=1649813566&width=560"},
+    {name: "Teo", image: "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/55295847/1/?bust=1649891796&width=560"},
     {name: "Harry", image: "https://img.dog-learn.com/dog-breeds/siberian-husky/siberian-husky-sz6.jpg"},
     {name: "Harry", image: "https://img.dog-learn.com/dog-breeds/siberian-husky/siberian-husky-sz6.jpg"}
 ];
